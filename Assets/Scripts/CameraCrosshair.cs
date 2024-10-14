0d2c9ad439eadc2864e226a27e6dd28d6bd47019
Phucego <phucdinhthanh2312@gmail.com>
2024-10-14 22:09:18 +0700

    14/10 selection outline stuff

---
+++ Assets/Scripts/CameraCrosshair.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class CameraCrosshair : MonoBehaviour
+{
+    public Camera cam;
+
+    public LayerMask towerLayer;
+
+    [SerializeField] private TowerLogic selectedTower;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            TowerInteraction();
+        }
+     
+    }
+
+    void TowerInteraction()
+    {
+        // Create a ray from the center of the screen
+        Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        RaycastHit hit;
+        
+      
+        // Check if the ray hits a tower within range
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, towerLayer))
+        {
+            TowerLogic tower = hit.collider.GetComponent<TowerLogic>();
+
+            if (tower != null)
+            {
+                Debug.Log("Interacted with tower: " + tower.name);
+                selectedTower = tower;
+               
+                tower.OnInteract();
+            }
+        }
+        else
+        {
+            Debug.Log("No tower hit!");
+            selectedTower = null; // Clear selection if nothing was hit
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/TowerDataSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Tower_DB", menuName = "Tower Data/Tower") ]
+public class TowerDataSO : ScriptableObject
+{
+    public string name;
+
+    public int hp;
+    
+}
