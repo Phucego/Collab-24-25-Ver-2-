3bbc632fd637e32bc950586e16a5e557c266fcd4
ImDaeBob <91824547+ImDaeBob@users.noreply.github.com>
2024-11-01 19:53:01 +0700

    Enum List Enemy Typing Implementation

--- Assets/Scripts/ScriptableObjects/BaseEnemySO.cs
+++ Assets/Scripts/ScriptableObjects/BaseEnemySO.cs
@@ -1,16 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
+public enum eType
+{
+    Peon,
+    Airborne,
+    Invisible,
+    Retaliate,
+    Mini_Boss,
+    Boss
+}
+
 [CreateAssetMenu(fileName = "EnemyData_DB", menuName = "New Enemy Data")]
 public class BaseEnemySO : ScriptableObject
 {
     public Color color = Color.white;
     public new string name;
     public string description;
-    public bool towerTarget = false;
-    public bool airborneType = false;
+    public eType[] type;
 
     public int maxHealth;
     public float maxSpeed;
-    
+
+    private void OnEnable()
+    {
+        if (type == null || type.Length == 0)
+            type = new eType[] { eType.Peon };
+    }
 }
