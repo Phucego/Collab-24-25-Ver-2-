0731f295cf3defa6bb757e6df1dde1cb586e6570
ImDaeBob <91824547+ImDaeBob@users.noreply.github.com>
2024-12-16 00:17:50 +0700

    Bugfixes and proper implementation

---
+++ Assets/Individuals/ImDaeBob Contents/Scripts/EnemyWave/WaveManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    [Header("Enemy Data:")]
+    [SerializeField] GameObject _enemyPrefab;
+    [SerializeField] BaseEnemySO[] _typeList;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+            SpawnEnemy("Tut_Path1", 0);
+        else if (Input.GetKeyDown(KeyCode.Keypad2))
+            SpawnEnemy("Tut_Path1", 1);
+        else if (Input.GetKeyDown(KeyCode.Keypad3))
+            SpawnEnemy("Tut_Path1", 2);
+        else if (Input.GetKeyDown(KeyCode.Keypad4))
+            SpawnEnemy("Tut_Path2", 0);
+        else if (Input.GetKeyDown(KeyCode.Keypad5))
+            SpawnEnemy("Tut_Path2", 1);
+        else if (Input.GetKeyDown(KeyCode.Keypad6))
+            SpawnEnemy("Tut_Path2", 2);
+    }
+
+    private void SpawnEnemy(string path, int type)
+    {
+        GameObject anEnemy = Instantiate(_enemyPrefab);
+
+        EnemyBehavior enemyBehavior = anEnemy.GetComponent<EnemyBehavior>();
+        enemyBehavior.data = _typeList[type];
+        enemyBehavior.SetStats();
+        enemyBehavior.SetPath(path);
+        Capture(anEnemy);
+
+        //Debug.Log("An Enemy has spawned at " + anEnemy.transform.position + "!");
+    }
+
+    private void Capture(GameObject obj) //Put the newly spawned enemy into the Pool which has this script!
+    {
+        obj.transform.parent = this.transform;
+    }
+}
